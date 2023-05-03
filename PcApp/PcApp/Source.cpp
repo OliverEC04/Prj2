6b121ed6d3ab904dda3c0dedebab29931101be02
antonbagger <antonboy123@gmail.com>
2023-05-03 10:53:50 +0200

    UART WORKING

--- PcApp/PcApp/Source.cpp
+++ PcApp/PcApp/Source.cpp
@@ -9,10 +9,11 @@
 
 bool sendString(int port, int baudRate, string data);
 char receiveChar();
+bool recieveSerial(CSerial& serial);
 
 void main()
 {
-	CSerial* s = new CSerial();
+	/*CSerial* s = new CSerial();
 
 	if (!s->Open(6, 9600))
 	{
@@ -22,33 +23,87 @@ void main()
 
 	s->SendData("7", UNIX_LENGTH);
 
-	s->SendData("3", UNIX_LENGTH);
+	s->SendData("3", UNIX_LENGTH);*/
 
-	char* lpBuffer = new char[3];
+	CSerial serial;
+
+	
 
 	while (1)
 	{
-		while (s->ReadDataWaiting() == 0)
-		{ }
+		if(recieveSerial(serial) == 1)
+			cout << "recieveSerial(serial)";
+
 		
-			s->ReadData(lpBuffer, 3);
+		/*serial.SendData("7", UNIX_LENGTH);
 
-			char buffer = *lpBuffer;
+		serial.SendData("3", UNIX_LENGTH);
 
-			if(buffer > 0 && buffer < 127)
-				cout << buffer[]
 
-			//arbejd videre med dette prøv med for-loop fx
+		if (serial.Open(6, 9600))
+		{
+				char* lpBuffer = new char[3];
+				int nBytesRead = serial.ReadData(lpBuffer, 3);
+				for (size_t i = 0; i < nBytesRead; i++)
+				{
+						cout << lpBuffer[i];
+				}
+				delete[]lpBuffer;
 			
-
-		
+		}
+		else
+			cout << "Failed to open port!" << endl;*/
 	}
 
-	delete[]lpBuffer;
+	
 
-	s->Close();
 
-	delete s;
+	//while (1)
+	//{
+	//	//while (s->ReadDataWaiting() == 0)
+	//	//{ }
+	//	//
+	//	//	s->ReadData(lpBuffer, 3);
+
+	//	//	char buffer = *lpBuffer;
+
+	//	//	if(buffer > 0 && buffer < 127)
+	//	//		cout << buffer[]
+
+	//	//	//arbejd videre med dette prøv med for-loop fx
+	//		
+
+	//	
+	//}
+
+	//delete[]lpBuffer;
+
+	/*s->Close();
+
+	delete s;*/
+}
+
+bool recieveSerial(CSerial& serial)
+{
+	if (serial.Open(6, 9600))
+	{
+		char* lpBuffer = new char[1];
+		int nBytesRead = serial.ReadData(lpBuffer, 1);
+		if (lpBuffer[0] > 0)
+		{
+			delete[]lpBuffer;
+			return true;
+		}
+		else
+		{
+			delete[]lpBuffer;
+			return false;
+		}
+	}
+	else
+		cout << "Failed to open port!" << endl;
+	
+	return 0;
 }
 
 bool sendString(int port, int baudRate, string data)
