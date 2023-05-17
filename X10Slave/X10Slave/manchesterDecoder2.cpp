9cc88b50a927af7ecaefc7e5a33ecda5e669150a
OliverEC <oliverec04@gmail.com>
2023-05-17 13:35:21 +0200

    byte manchester v1

--- X10Slave/X10Slave/manchesterDecoder2.cpp
+++ X10Slave/X10Slave/manchesterDecoder2.cpp
@@ -4,59 +4,53 @@
  * Created: 17-05-2023 11:01:58
  *  Author: olive
  */ 
-#include <string.h>
 
+#define F_CPU 16000000
 
-#define ADDRESS_SIZE 4
+#include <util/delay.h>
 
-char* testInput = "111010100101";
-char defuck = 0;
+#include "Led.h"
 
-void manchesterDecoder2(char* inputBits, char* outputBuffer, char inputSize, char outputSize)
+// 10100101
+
+// HUSK AT LÆS FRA HØJRE !!!!!!!!!!!!!!
+
+char manchesterDecoder2(char addressByte, char dataByte)
 {
-	char addressBits[5] = "1110";
-	//strncpy(addressBits, inputBits, ADDRESS_SIZE);
+	char decoded = addressByte << 4;
 	
-	char dataBits[9] = "10100101";
-	//strncpy(dataBits, inputBits + ADDRESS_SIZE, inputSize - ADDRESS_SIZE);
-	
-	char decodedBits[5] = "0000";
-	
-	if (strcmp(addressBits, "1110") == 0)
+	if (addressByte == 0b00000111)
 	{
-		char ite = 0;
-		for (char i = 0; i < inputSize - ADDRESS_SIZE - 1; i += 2)
+		for (char i = 0; i < 4; i++)
 		{
-			if (dataBits[i] == '0' && dataBits[i + 1] == '1')
+			if ((dataByte |= 0b00000010) && (dataByte &= 0b11111110)) // if 10
 			{
-				char zero = '0';
-				decodedBits[ite] = '0';
+				decoded &= 0b11111110 << i; // add 0 to end
 			}
-			else if (dataBits[i] == '1' && dataBits[i + 1] == '0')
+			else if ((dataByte &= 0b11111101) && (dataByte |= 0b00000001)) // if 01
 			{
-				char one = '1';
-				decodedBits[ite] = '1';
-			}
-			else
-			{
-				defuck = 3;
+				decoded |= 0b00000001 << i; // add 1 to end
 			}
 			
-			ite++;
+			dataByte >> 2; // Move 2 right
+			
+			writeAllLEDs(dataByte);
+			_delay_ms(2000);
 		}
 	}
-	else
-	{
-		defuck = 4;
-	}
 	
-	outputBuffer = decodedBits;
+	return decoded;
 }
 
 int main(void)
 {
-	char myOutput[5];
-	manchesterDecoder2(testInput, myOutput, 13, 5);
+	initLEDport();
+	
+	char myDecode;
+	
+	myDecode = manchesterDecoder2(0b00000111, 0b10100101);
+	
+	writeAllLEDs(myDecode);
 	
 	return 0;
 }
