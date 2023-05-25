c968a81bc4c4e8b2da0c4f4c353966b7a73664f9
OliverEC <oliverec04@gmail.com>
2023-05-25 11:16:19 +0200

    main cleanup

--- PcApp/PcApp/Source.cpp
+++ PcApp/PcApp/Source.cpp
@@ -3,78 +3,14 @@
 #include <conio.h>
 #include <iostream>
 #include <string>
-#define length 1
 
-//bool recieveSerial(CSerial& serial);
 
 int main()
 {
-	string input;
 	Ui ui;
-
-	//string config = "0025299999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999900148";
 
 	while (1)
 	{
-		/*if (recieveSerial(serial) == 1)
-			cout << "recieveSerial(serial)";
-
-		cin >> input;
-
-		if (input.find("AlarmTime") != string::npos)
-		{
-			int spacePos = input.find(':');
-			string timeStr = input.substr(spacePos + 1, spacePos + 5);
-
-			int ourTime = stoi(timeStr);
-
-			week.days_[0].setAlarmTime(ourTime);
-		}
-
-
-		week.sendConfig(serial);*/
-
-
-
-		/*switch (_getch())
-		{
-		case 'f':
-			if (!s->Open(7, 9600))
-			{
-				cout << "Failed to open port!" << endl;
-			}
-			else
-			{
-				s->SendData(config.c_str(), 140);
-				cout << config.c_str();
-			}
-			break;
-		}*/
-		
-
 		ui.update();
 	}
 }
-
-//bool recieveSerial(CSerial& serial)
-//{
-//	if (serial.Open(10, 9600))
-//	{
-//		char* lpBuffer = new char[1];
-//		int nBytesRead = serial.ReadData(lpBuffer, 1);
-//		if (lpBuffer[0] > 0)
-//		{
-//			delete[]lpBuffer;
-//			return true;
-//		}
-//		else
-//		{
-//			delete[]lpBuffer;
-//			return false;
-//		}
-//	}
-//	else
-//		cout << "Failed to open port!" << endl;
-//	
-//	return 0;
-//}
